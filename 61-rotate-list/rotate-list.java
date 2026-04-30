420644c388f842f00bd11d309941cbff3a4575ec
SHANMUKHROHITH-01 <shanmukhrohithalakki@gmail.com>
2026-04-30 09:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (25.53%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode temp = head;
+        int length = 1;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        k = k % length;
+
+        if (k == 0) {
+            return head;
+        }
+
+        temp.next = head;
+
+        int steps = length - k;
+        ListNode newTail = head;
+
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+
+        newTail.next = null;
+
+        return newHead;
+    }
+}
+      
+    
